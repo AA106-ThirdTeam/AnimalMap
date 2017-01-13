c2a25d4ddeba4e73cf5f748ece2f5e50f1ec2d95
LinWeiHan <shps951002@gmail.com>
2017-01-13 21:02:44 +0800

    debug:http://stackoverflow.com/questions/24416569/eclipse-project-cannot-reference-itself

--- WebContent/test/0113/js/index.js
+++ WebContent/test/0113/js/index.js
@@ -20,17 +20,19 @@ map.tinyMap({
 	 */	
 	event : function(map) {
 		console.log('點擊地圖');
+		map.forEach(myFunction);
 	}
 });
 
 
 var btn_test = $("#btn_test");
 $(btn_test).click(function(){
-	alert(map)
 	map.tinyMap('modify',{
-	    'zoom':16,
 		'marker': [
 		   {'addr': ['25.042152', '121.535398'],'text': '台北科大正門'}
-		]
-	});
-});
+		],//'marker'
+		'marker': [
+		   {'addr': ['25.0383270525352', '121.57045841217041'],'text': '正門'}
+		]//'marker'
+	});//map.tinyMap
+});//$(btn_test).click(function(){

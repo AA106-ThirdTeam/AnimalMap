6ee7cd80aeeeea9dcb7e940b83543d1f9b931803
LinWeiHan <shps951002@gmail.com>
2017-01-19 14:20:43 +0800

    多個PK暫時解決法 ver01

--- src/utils/excel2sql/Excel_to_SQL.java
+++ src/utils/excel2sql/Excel_to_SQL.java
@@ -18,25 +18,184 @@ public class Excel_to_SQL {
 	private static final String USER = "TEST_ER_MODEL";
 	private static final String PASSWORD = "food1234";
 	
-	public static void sql_drop(Workbook workbook) {
+	public static void sql_drop(String tableName) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection(URL, USER, PASSWORD);
+			String str = "";
+			String str1 = "drop table ";
+			str = str1 + tableName + " CASCADE CONSTRAINTS ";//用java下sql指令 不用加;
+			//====打印====
+			System.out.println(str+";");
+			//====SQL====
+			pstmt = con.prepareStatement(str);
+			pstmt.executeUpdate();
+			//====給文本使用，要加;====
+			str +=  ";";//用java下sql指令 不用加;
+			
+		} catch (ClassNotFoundException ce) {
+			System.out.println(ce);
+		} catch (SQLException se) {
+			System.out.println(se);
+		} finally {
+			// 依建立順序關閉資源 (越晚建立越早關閉)
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					System.out.println(se);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException se) {
+					System.out.println(se);
+				}
+			}
+		}		
+
+	}
+	static String 欄位名稱_PK2 = null ;
+	public static void sql_create(String tableName,Workbook workbook,int start,int end) {
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			con = DriverManager.getConnection(URL, USER, PASSWORD);
 			
+			String str = "";
+			String str1 = "CREATE TABLE ";
+			String 表格名稱 = tableName;
+			str = str1 + 表格名稱 + " (";
+			// =====================
+			for (int k = start; k < end; k++) {
+				String tempStr = "";
+				String 欄位名稱 = sheet.getCell(1, k).getContents();
+				//暫時解決:多個PK問題
+				boolean flog_b = false;
+				boolean flog_b2 = false;
+				String tmpStr1 = sheet.getCell(5, k).getContents();
+				String tmpStr2 = sheet.getCell(5, k+1).getContents();
+//				System.err.println(tmpStr1);
+//				System.err.println(tmpStr1);
+				if (
+						!(tmpStr1.indexOf("PK")==-1)
+						&&!(tmpStr2.indexOf("PK")==-1)
+						) {
+					欄位名稱_PK2 = sheet.getCell(1, k+1).getContents();
+					flog_b = true;
+					flog_b2 = true;
+				}
+				
+				String 資料型態 = sheet.getCell(3, k).getContents();
+				String 欄位長度 = sheet.getCell(4, k).getContents();
+				String 限制條件 = sheet.getCell(5, k).getContents();
+				
+				// Cell的getContents方法把單元格中的信息以字符的形式讀取出來
+				if (k == start) {
+					tempStr += "";
+				}else{
+					tempStr += ",";
+				}
+				tempStr += 欄位名稱 + " " 
+						+ 資料型態 + ""
+						;
+				if ("DATE".equals(資料型態) || "BLOB".equals(資料型態)) {
+					tempStr += 欄位長度;
+				}else{
+					tempStr += "(" + 欄位長度 + ")"; 
+				}
+				
+				if (!(限制條件.equals("Not Null"))&& !flog_b) {
+					if (!(限制條件.indexOf("PK")==-1)) {
+						String tempStr2 = ((flog_b2) ? 欄位名稱_PK2 : "");
+						tempStr += ", ConStraint "+表格名稱+"_PK PRIMARY KEY ( " 
+								+ tempStr2
+								+ 欄位名稱
+								+ " ) ENABLE"
+								;
+						if(flog_b2){
+							System.err.println(tempStr);
+						}
+						flog_b2 = false;
+					}else{
+						tempStr += " NOT NULL ";
+					}
+				}
+				flog_b = false;
+				str += tempStr;
+			}
 			
-			for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
+			// =====================
+			str += " )";
+			//====打印====
+			System.out.println(str+";");
+			//====SQL====
+			pstmt = con.prepareStatement(str);
+		    pstmt.executeUpdate();
+			//====給文本使用，要加;====
+			str += ";";
+		} catch (ClassNotFoundException ce) {
+			System.out.println(ce);
+		} catch (SQLException se) {
+			System.out.println(se);
+		} finally {
+			// 依建立順序關閉資源 (越晚建立越早關閉)
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					System.out.println(se);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException se) {
+					System.out.println(se);
+				}
+			}
+		}		
+	}
+	
+	public static void sql_note(String tableName,Workbook workbook,int start,int end) {	
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection(URL, USER, PASSWORD);
+					
+			// =====================
+			for (int k = start; k < end; k++) {
 				String str = "";
-				String str1 = "drop table ";
-				String 表格名稱 = workbook.getSheet(i).getName();
-				str = str1 + 表格名稱 + " ";//用java下sql指令 不用加;
+				String str1 = "COMMENT ON COLUMN ";
+				String 表格名稱 = tableName;
+				str = str1 + 表格名稱 + ".";				
+				String 欄位名稱 = sheet.getCell(1, k).getContents();
+				String 註解;
+				String tempStr = str + 欄位名稱 + " IS " ;
+				
+				if (sheet.getCell(4, k)==null){
+					註解 = "";
+				}
+				else{
+					
+					註解 =  
+							"'" + sheet.getCell(2, k).getContents() + " | PS: "
+							+ sheet.getCell(8, k).getContents()
+							+ "'";	
+				}
+				tempStr += 註解 + "";
+				//====打印====
+				System.out.println(tempStr+";");
 				//====SQL====
-				pstmt = con.prepareStatement(str);
+				pstmt = con.prepareStatement(tempStr);
 				pstmt.executeUpdate();
 				//====給文本使用，要加;====
-				str +=  ";";//用java下sql指令 不用加;
-				System.out.println(str);
+				tempStr += ";";
 			}
 		} catch (ClassNotFoundException ce) {
 			System.out.println(ce);
@@ -50,159 +209,7 @@ public class Excel_to_SQL {
 				} catch (SQLException se) {
 					System.out.println(se);
 				}
-			}
-			if (con != null) {
-				try {
-					con.close();
-				} catch (SQLException se) {
-					System.out.println(se);
-				}
-			}
-		}		
-
-	}
-
-	public static void sql_create(Workbook workbook) {
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			con = DriverManager.getConnection(URL, USER, PASSWORD);
 				
-			for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
-				String str = "";
-				String str1 = "CREATE TABLE ";
-				String 表格名稱 = workbook.getSheet(i).getName();
-				str = str1 + 表格名稱 + " (";
-	
-				// 由Workbook的getSheet方法選擇工作表（從0開始）
-				Sheet sheet = workbook.getSheet(i);
-				// 取得Sheet表中所包含的總row數
-				int rows = sheet.getRows();
-	
-				// =====================
-				for (int k = 1; k < rows; k++) {
-					String tempStr = "";
-					String 欄位名稱 = sheet.getCell(0, k).getContents();
-					String 資料型態 = sheet.getCell(1, k).getContents();
-					String 欄位長度 = sheet.getCell(2, k).getContents();
-					String 限制條件 = sheet.getCell(3, k).getContents();
-					
-					// Cell的getContents方法把單元格中的信息以字符的形式讀取出來
-					if (k == 1) {
-						tempStr += "";
-					}else{
-						tempStr += ",";
-					}
-					tempStr += 欄位名稱 + " " 
-							+ 資料型態 + ""
-							;
-					if ("DATE".equals(資料型態) || "BLOB".equals(資料型態)) {
-						tempStr += 欄位長度;
-					}else{
-						tempStr += "(" + 欄位長度 + ")"; 
-					}
-					
-					if (!(限制條件.equals("Not Null"))) {
-						if (!(限制條件.indexOf("PK")==-1)) {
-							tempStr += ", ConStraint "+表格名稱+"_PK PRIMARY KEY ( " 
-									+ 欄位名稱 
-									+ " ) ENABLE"
-									;
-//							System.err.println("PK");
-						}else{
-							tempStr += " NOT NULL ";
-						}
-					}
-					str += tempStr;
-				}
-	
-				// =====================
-				str += " )";
-				//System.out.println(str);
-				//====SQL====
-				pstmt = con.prepareStatement(str);
-			    pstmt.executeUpdate();
-				//====給文本使用，要加;====
-				str += ";";
-				System.out.println(str);				
-			}
-		} catch (ClassNotFoundException ce) {
-			System.out.println(ce);
-		} catch (SQLException se) {
-			System.out.println(se);
-		} finally {
-			// 依建立順序關閉資源 (越晚建立越早關閉)
-			if (pstmt != null) {
-				try {
-					pstmt.close();
-				} catch (SQLException se) {
-					System.out.println(se);
-				}
-			}
-			if (con != null) {
-				try {
-					con.close();
-				} catch (SQLException se) {
-					System.out.println(se);
-				}
-			}
-		}		
-	}
-	
-	public static void sql_note(Workbook workbook) {	
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			con = DriverManager.getConnection(URL, USER, PASSWORD);
-					
-			for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
-				// 由Workbook的getSheet方法選擇工作表（從0開始）
-				Sheet sheet = workbook.getSheet(i);
-				// 取得Sheet表中所包含的總row數
-				int rows = sheet.getRows();			
-				// =====================
-				for (int k = 1; k < rows; k++) {
-					String str = "";
-					String str1 = "COMMENT ON COLUMN ";
-					String 表格名稱 = workbook.getSheet(i).getName();
-					str = str1 + 表格名稱 + ".";				
-					String 欄位名稱 = sheet.getCell(0, k).getContents();
-					String 註解;
-					String tempStr = str + 欄位名稱 + " IS " ;
-					
-					if (sheet.getCell(4, k)==null){
-						註解 = "";
-					}
-					else{
-						
-						註解 =  
-								"'" + sheet.getCell(4, k).getContents() + " | PS: "
-								+ sheet.getCell(7, k).getContents()
-								+ "'";	
-					}
-					tempStr += 註解 + "";
-					//====SQL====
-					pstmt = con.prepareStatement(tempStr);
-					pstmt.executeUpdate();
-					//====給文本使用，要加;====
-					tempStr += ";";
-					System.out.println(tempStr);				
-				}
-			}
-		} catch (ClassNotFoundException ce) {
-			System.out.println(ce);
-		} catch (SQLException se) {
-			System.out.println(se);
-		} finally {
-			// 依建立順序關閉資源 (越晚建立越早關閉)
-			if (pstmt != null) {
-				try {
-					pstmt.close();
-				} catch (SQLException se) {
-					System.out.println(se);
-				}
 			}
 			if (con != null) {
 				try {
@@ -220,49 +227,42 @@ public class Excel_to_SQL {
 	 * @version 1.0
 	 * @author 暐翰
 	 */
-	public static void sql_fk(Workbook workbook) {
+	public static void sql_fk(String tableName,Workbook workbook,int start,int end) {
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		int count_Fk = 1;
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			con = DriverManager.getConnection(URL, USER, PASSWORD);
+
+			// =====================
+			for (int k = start; k < end; k++) {
+				String 欄位名稱 = sheet.getCell(1, k).getContents();
+				String 限制條件 = sheet.getCell(5, k).getContents();
+				String 對應表格 = sheet.getCell(6, k).getContents();
+				String 對應欄位 = sheet.getCell(7, k).getContents();
 				
-			for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
-	
-				// 由Workbook的getSheet方法選擇工作表（從0開始）
-				Sheet sheet = workbook.getSheet(i);
-				// 取得Sheet表中所包含的總row數
-				int rows = sheet.getRows();
-	
-				// =====================
-				for (int k = 1; k < rows; k++) {
-					String 欄位名稱 = sheet.getCell(0, k).getContents();
-					String 限制條件 = sheet.getCell(3, k).getContents();
-					String 對應表格 = sheet.getCell(5, k).getContents();
-					String 對應欄位 = sheet.getCell(6, k).getContents();
-					
-					if (!(限制條件.indexOf("FK")==-1)) {
-						String str = "";
-						String 表格名稱 = workbook.getSheet(i).getName();
-						str += 	
-								"ALTER TABLE "
-								+ 表格名稱 
-								+ " ADD CONSTRAINT " + 表格名稱 +"_FK"+(count_Fk++) 
-								+ " FOREIGN KEY ( " + 欄位名稱 + " )"
-								+ " REFERENCES " + 對應表格 + " ( " + 對應欄位 +" ) "
-								+ "ENABLE"
-								;
-						//====SQL====
-						pstmt = con.prepareStatement(str);
-					    pstmt.executeUpdate();
-						//====給文本使用，要加;====
-						str += ";";
-						System.out.println(str);				
-					}						
-				}
-	
+				if (!(限制條件.indexOf("FK")==-1)) {
+					String str = "";
+					String 表格名稱 = tableName;
+					str += 	
+							"ALTER TABLE "
+							+ 表格名稱 
+							+ " ADD CONSTRAINT " + 表格名稱 +"_FK"+(count_Fk++) 
+							+ " FOREIGN KEY ( " + 欄位名稱 + " )"
+							+ " REFERENCES " + 對應表格 + " ( " + 對應欄位 +" ) "
+							+ "ENABLE"
+							;
+					//====打印====
+					System.out.println(str+";");
+					//====SQL====
+					pstmt = con.prepareStatement(str);
+				    pstmt.executeUpdate();
+					//====給文本使用，要加;====
+					str += ";";
+				}						
 			}
+	
 		} catch (ClassNotFoundException ce) {
 			System.out.println(ce);
 		} catch (SQLException se) {
@@ -292,45 +292,37 @@ public class Excel_to_SQL {
 	 * @version 1.0
 	 * @author 暐翰
 	 */
-	public static void sql_unique(Workbook workbook) {
+	public static void sql_unique(String tableName,Workbook workbook,int start,int end) {
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		int count_UK = 1;
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			con = DriverManager.getConnection(URL, USER, PASSWORD);
+	
+			// =====================
+			for (int k = start; k < end; k++) {
+				String 欄位名稱 = sheet.getCell(1, k).getContents();
+				String 限制條件 = sheet.getCell(5, k).getContents();
 				
-			for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
-	
-				// 由Workbook的getSheet方法選擇工作表（從0開始）
-				Sheet sheet = workbook.getSheet(i);
-				// 取得Sheet表中所包含的總row數
-				int rows = sheet.getRows();
-	
-				// =====================
-				for (int k = 1; k < rows; k++) {
-					String 欄位名稱 = sheet.getCell(0, k).getContents();
-					String 限制條件 = sheet.getCell(3, k).getContents();
-					
-					if (!(限制條件.indexOf("UNIQUE")==-1)) {
-						String str = "";
-						String 表格名稱 = workbook.getSheet(i).getName();
-						str += 	
-								"ALTER TABLE "
-								+ 表格名稱 
-								+ " ADD CONSTRAINT " + 表格名稱 +"_UK"+(count_UK++) 
-								+ " UNIQUE ( " + 欄位名稱 + " )"
-								+ "ENABLE"
-								;
-						//====SQL====
-						pstmt = con.prepareStatement(str);
-					    pstmt.executeUpdate();
-						//====給文本使用，要加;====
-						str += ";";
-						System.out.println(str);				
-					}						
-				}
-	
+				if (!(限制條件.indexOf("UNIQUE")==-1)) {
+					String str = "";
+					String 表格名稱 = tableName;
+					str += 	
+							"ALTER TABLE "
+							+ 表格名稱 
+							+ " ADD CONSTRAINT " + 表格名稱 +"_UK"+(count_UK++) 
+							+ " UNIQUE ( " + 欄位名稱 + " )"
+							+ "ENABLE"
+							;
+					//====打印====
+					System.out.println(str+";");
+					//====SQL====
+					pstmt = con.prepareStatement(str);
+				    pstmt.executeUpdate();
+					//====給文本使用，要加;====
+					str += ";";
+				}						
 			}
 		} catch (ClassNotFoundException ce) {
 			System.out.println(ce);
@@ -355,52 +347,84 @@ public class Excel_to_SQL {
 		}		
 	}	
 	
+	static List<Integer> startList ;
+	static List<Integer> endList ;
+	static List<String> tableNameList ;	
+	static Sheet sheet;
+	static int 最大欄的數量 = 8;
+	
 	public static void excel_main() throws IOException, BiffException {
-		File 暐翰表格 = new File("C:\\Users\\Administrator\\git\\SQL_For_AnimalMapTable\\data\\暐翰的表格.xls");
-		File 志鈞表格 = new File("C:\\Users\\Administrator\\git\\SQL_For_AnimalMapTable\\data\\志鈞的表格.xls");
-
-		List<File> list = new ArrayList<File>();
-		list.add(志鈞表格);
-		list.add(暐翰表格);
-		Workbook workbook = null;
+		File file = new File("E:/ER-model (10).xls");
+		Workbook workbook = Workbook.getWorkbook(file);
 		
-		for (Object 表格 : list) {			
-			// 從輸入流創建Workbook
-			workbook = Workbook.getWorkbook((File)表格);
+		int 工作簿數量 = workbook.getNumberOfSheets();
+		int flog_int = 0;
+		boolean flog_b = false;
+		System.out.println(工作簿數量);
+		for (int i = 2; i < 4; i++) {
+			startList = new ArrayList<Integer>();
+			endList = new ArrayList<Integer>();
+			tableNameList = new ArrayList<String>();
+			sheet = workbook.getSheet(i);
+			int 列的數量 = sheet.getRows();
 			
-			{// 刪除table
-				sql_drop(workbook);
+			for (int j = 0; j < 列的數量; j++) {
+				String val = sheet.getCell(0, j).getContents();
+				if ("表格名稱".equals(val)) {
+					flog_b = true;
+					startList.add(j);//紀錄開始點
+//					System.err.println("開始:"+j);
+					String tableName = sheet.getCell(1, j+1).getContents();
+					tableNameList.add(tableName);
+				}
+				if (flog_b && "".equals(val)||" ".equals(val)){
+					flog_int++;
+					if (flog_int==2){
+						flog_b = false;
+						flog_int = 0;
+						endList.add(j);//紀錄結束點
+//						System.err.println("結束:"+j);
+					}
+				}
+//				System.out.println(sheet.getCell(0, j).getContents());
 			}
 			
-			{// 建立table
-				sql_create(workbook);
+			//==============
+			for (int k = 0; k < tableNameList.size(); k++) {
+				{// 刪除table
+					sql_drop((String)tableNameList.get(k));
+				}	
 			}
-			
-			{// 建立註解
-				sql_note(workbook);
-			}		
-
-			{// 建立UNIQUE
-				sql_unique(workbook);
+			for (int k = 0; k < tableNameList.size(); k++) {		
+				{// 建立table
+					sql_create((String)tableNameList.get(k),workbook,(int)startList.get(k)+4,(int)endList.get(k));
+				}	
+			}
+			for (int k = 0; k < tableNameList.size(); k++) {		
+				{// 建立備註
+					sql_note((String)tableNameList.get(k),workbook,(int)startList.get(k)+4,(int)endList.get(k));
+				}	
+			}
+			for (int k = 0; k < tableNameList.size(); k++) {		
+				{// 建立FK
+					sql_fk((String)tableNameList.get(k),workbook,(int)startList.get(k)+4,(int)endList.get(k));
+				}	
+			}	
+			for (int k = 0; k < tableNameList.size(); k++) {		
+				{// 建立Unique
+					sql_unique((String)tableNameList.get(k),workbook,(int)startList.get(k)+4,(int)endList.get(k));
+				}	
 			}				
-			
-			{// 建立FK
-				sql_fk(workbook);
-			}		
-			
-			
-			workbook.close();
 		}
+
 	}
 
 	public static void main(String[] args) {
 		try {
 			excel_main();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (BiffException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
---
+++ src/utils/excel2sql/Excel_to_SQL_ver01.java
@@ -0,0 +1,409 @@
+package utils.excel2sql;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+public class Excel_to_SQL_ver01 {
+	private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+	private static final String USER = "TEST_ER_MODEL";
+	private static final String PASSWORD = "food1234";
+	
+	public static void sql_drop(Workbook workbook) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection(URL, USER, PASSWORD);
+			
+			
+			for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
+				String str = "";
+				String str1 = "drop table ";
+				String 表格名稱 = workbook.getSheet(i).getName();
+				str = str1 + 表格名稱 + " CASCADE CONSTRAINTS ";//用java下sql指令 不用加;
+				//====SQL====
+				pstmt = con.prepareStatement(str);
+				pstmt.executeUpdate();
+				//====給文本使用，要加;====
+				str +=  ";";//用java下sql指令 不用加;
+				System.out.println(str);
+			}
+		} catch (ClassNotFoundException ce) {
+			System.out.println(ce);
+		} catch (SQLException se) {
+			System.out.println(se);
+		} finally {
+			// 依建立順序關閉資源 (越晚建立越早關閉)
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					System.out.println(se);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException se) {
+					System.out.println(se);
+				}
+			}
+		}		
+
+	}
+
+	public static void sql_create(Workbook workbook) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection(URL, USER, PASSWORD);
+				
+			for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
+				String str = "";
+				String str1 = "CREATE TABLE ";
+				String 表格名稱 = workbook.getSheet(i).getName();
+				str = str1 + 表格名稱 + " (";
+	
+				// 由Workbook的getSheet方法選擇工作表（從0開始）
+				Sheet sheet = workbook.getSheet(i);
+				// 取得Sheet表中所包含的總row數
+				int rows = sheet.getRows();
+	
+				// =====================
+				for (int k = 1; k < rows; k++) {
+					String tempStr = "";
+					String 欄位名稱 = sheet.getCell(0, k).getContents();
+					String 資料型態 = sheet.getCell(1, k).getContents();
+					String 欄位長度 = sheet.getCell(2, k).getContents();
+					String 限制條件 = sheet.getCell(3, k).getContents();
+					
+					// Cell的getContents方法把單元格中的信息以字符的形式讀取出來
+					if (k == 1) {
+						tempStr += "";
+					}else{
+						tempStr += ",";
+					}
+					tempStr += 欄位名稱 + " " 
+							+ 資料型態 + ""
+							;
+					if ("DATE".equals(資料型態) || "BLOB".equals(資料型態)) {
+						tempStr += 欄位長度;
+					}else{
+						tempStr += "(" + 欄位長度 + ")"; 
+					}
+					
+					if (!(限制條件.equals("Not Null"))) {
+						if (!(限制條件.indexOf("PK")==-1)) {
+							tempStr += ", ConStraint "+表格名稱+"_PK PRIMARY KEY ( " 
+									+ 欄位名稱 
+									+ " ) ENABLE"
+									;
+//							System.err.println("PK");
+						}else{
+							tempStr += " NOT NULL ";
+						}
+					}
+					str += tempStr;
+				}
+	
+				// =====================
+				str += " )";
+				//System.out.println(str);
+				//====SQL====
+				pstmt = con.prepareStatement(str);
+			    pstmt.executeUpdate();
+				//====給文本使用，要加;====
+				str += ";";
+				System.out.println(str);				
+			}
+		} catch (ClassNotFoundException ce) {
+			System.out.println(ce);
+		} catch (SQLException se) {
+			System.out.println(se);
+		} finally {
+			// 依建立順序關閉資源 (越晚建立越早關閉)
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					System.out.println(se);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException se) {
+					System.out.println(se);
+				}
+			}
+		}		
+	}
+	
+	public static void sql_note(Workbook workbook) {	
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection(URL, USER, PASSWORD);
+					
+			for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
+				// 由Workbook的getSheet方法選擇工作表（從0開始）
+				Sheet sheet = workbook.getSheet(i);
+				// 取得Sheet表中所包含的總row數
+				int rows = sheet.getRows();			
+				// =====================
+				for (int k = 1; k < rows; k++) {
+					String str = "";
+					String str1 = "COMMENT ON COLUMN ";
+					String 表格名稱 = workbook.getSheet(i).getName();
+					str = str1 + 表格名稱 + ".";				
+					String 欄位名稱 = sheet.getCell(0, k).getContents();
+					String 註解;
+					String tempStr = str + 欄位名稱 + " IS " ;
+					
+					if (sheet.getCell(4, k)==null){
+						註解 = "";
+					}
+					else{
+						
+						註解 =  
+								"'" + sheet.getCell(4, k).getContents() + " | PS: "
+								+ sheet.getCell(7, k).getContents()
+								+ "'";	
+					}
+					tempStr += 註解 + "";
+					//====SQL====
+					pstmt = con.prepareStatement(tempStr);
+					pstmt.executeUpdate();
+					//====給文本使用，要加;====
+					tempStr += ";";
+					System.out.println(tempStr);				
+				}
+			}
+		} catch (ClassNotFoundException ce) {
+			System.out.println(ce);
+		} catch (SQLException se) {
+			System.out.println(se);
+		} finally {
+			// 依建立順序關閉資源 (越晚建立越早關閉)
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					System.out.println(se);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException se) {
+					System.out.println(se);
+				}
+			}
+		}				
+	}
+	
+	/**
+	 * <h1>FK的操作</h1>
+	 * @param workbook
+	 * @version 1.0
+	 * @author 暐翰
+	 */
+	public static void sql_fk(Workbook workbook) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		int count_Fk = 1;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection(URL, USER, PASSWORD);
+				
+			for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
+	
+				// 由Workbook的getSheet方法選擇工作表（從0開始）
+				Sheet sheet = workbook.getSheet(i);
+				// 取得Sheet表中所包含的總row數
+				int rows = sheet.getRows();
+	
+				// =====================
+				for (int k = 1; k < rows; k++) {
+					String 欄位名稱 = sheet.getCell(0, k).getContents();
+					String 限制條件 = sheet.getCell(3, k).getContents();
+					String 對應表格 = sheet.getCell(5, k).getContents();
+					String 對應欄位 = sheet.getCell(6, k).getContents();
+					
+					if (!(限制條件.indexOf("FK")==-1)) {
+						String str = "";
+						String 表格名稱 = workbook.getSheet(i).getName();
+						str += 	
+								"ALTER TABLE "
+								+ 表格名稱 
+								+ " ADD CONSTRAINT " + 表格名稱 +"_FK"+(count_Fk++) 
+								+ " FOREIGN KEY ( " + 欄位名稱 + " )"
+								+ " REFERENCES " + 對應表格 + " ( " + 對應欄位 +" ) "
+								+ "ENABLE"
+								;
+						//====SQL====
+						pstmt = con.prepareStatement(str);
+					    pstmt.executeUpdate();
+						//====給文本使用，要加;====
+						str += ";";
+						System.out.println(str);				
+					}						
+				}
+	
+			}
+		} catch (ClassNotFoundException ce) {
+			System.out.println(ce);
+		} catch (SQLException se) {
+			System.out.println(se);
+		} finally {
+			// 依建立順序關閉資源 (越晚建立越早關閉)
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					System.out.println(se);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException se) {
+					System.out.println(se);
+				}
+			}
+		}		
+	}
+
+	/**
+	 * <h1>UK的操作</h1>
+	 * @param workbook
+	 * @version 1.0
+	 * @author 暐翰
+	 */
+	public static void sql_unique(Workbook workbook) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		int count_UK = 1;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection(URL, USER, PASSWORD);
+				
+			for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
+	
+				// 由Workbook的getSheet方法選擇工作表（從0開始）
+				Sheet sheet = workbook.getSheet(i);
+				// 取得Sheet表中所包含的總row數
+				int rows = sheet.getRows();
+	
+				// =====================
+				for (int k = 1; k < rows; k++) {
+					String 欄位名稱 = sheet.getCell(0, k).getContents();
+					String 限制條件 = sheet.getCell(3, k).getContents();
+					
+					if (!(限制條件.indexOf("UNIQUE")==-1)) {
+						String str = "";
+						String 表格名稱 = workbook.getSheet(i).getName();
+						str += 	
+								"ALTER TABLE "
+								+ 表格名稱 
+								+ " ADD CONSTRAINT " + 表格名稱 +"_UK"+(count_UK++) 
+								+ " UNIQUE ( " + 欄位名稱 + " )"
+								+ "ENABLE"
+								;
+						//====SQL====
+						pstmt = con.prepareStatement(str);
+					    pstmt.executeUpdate();
+						//====給文本使用，要加;====
+						str += ";";
+						System.out.println(str);				
+					}						
+				}
+	
+			}
+		} catch (ClassNotFoundException ce) {
+			System.out.println(ce);
+		} catch (SQLException se) {
+			System.out.println(se);
+		} finally {
+			// 依建立順序關閉資源 (越晚建立越早關閉)
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					System.out.println(se);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException se) {
+					System.out.println(se);
+				}
+			}
+		}		
+	}	
+	
+	public static void excel_main() throws IOException, BiffException {
+		File 志鈞表格 = new File("C:/Users/Administrator/git/AnimalMap/src/oracle_sql命令/志鈞的表格.xls");
+		File 暐翰表格 = new File("C:/Users/Administrator/git/AnimalMap/src/oracle_sql命令/暐翰的表格.xls");
+
+		List<File> list = new ArrayList<File>();
+		list.add(志鈞表格);
+		list.add(暐翰表格);
+		Workbook workbook = null;
+		
+		for (Object 表格 : list) {			
+			// 從輸入流創建Workbook
+			workbook = Workbook.getWorkbook((File)表格);
+			
+			{// 刪除table
+				sql_drop(workbook);
+			}
+			
+			{// 建立table
+				sql_create(workbook);
+			}
+			
+			{// 建立註解
+				sql_note(workbook);
+			}		
+
+			{// 建立UNIQUE
+				sql_unique(workbook);
+			}				
+			
+			{// 建立FK
+				sql_fk(workbook);
+			}		
+			
+			
+			workbook.close();
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			excel_main();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BiffException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}

87f33205c1b37fa80e1b4990253d20c7f52f3a7e
ShadowRJ <justlovedance@gmail.com>
2017-02-22 17:17:30 +0800

    志鈞 PACKAGE 匯入

---
+++ src/com/emg_H/model/Emg_HDAO_interface.java
@@ -0,0 +1,22 @@
+package com.emg_H.model;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.emg_H_Msg.model.Emg_H_MsgVO;
+
+
+
+
+public interface Emg_HDAO_interface{
+
+	 public void insert(Emg_HVO emg_HVO);  
+     public void delete(String emg_H_Id);     
+     public Emg_HVO findByPrimaryKey(String emg_H_Id);
+     public List<Emg_HVO> getAll();
+     
+     //查求救留言透過求救編號 (一對多)
+      public Set<Emg_H_MsgVO> getEmg_H_MsgByEmg_H_Id(String emg_H_Id);
+     
+}
